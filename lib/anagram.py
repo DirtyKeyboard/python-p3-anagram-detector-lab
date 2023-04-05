be3993d87dda09dc3614d9b41d3e415102167545
DirtyKeyboard <andrewxx000222@icloud.com>
2023-04-05 11:14:32 -0500

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list):
+        ret = []
+        for el in list:
+            if sorted(el) == sorted([letter for letter in self.word]):
+                ret.append(el)
+        return ret
